119279f4251fc2437d83a89f6bffe52e7631a5fb
Willian Antunes <willian.lima.antunes@gmail.com>
2021-01-26 18:48:13 -0300

    How to setup a Gatsby plugin like Typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,17 @@
  * Configure your Gatsby site with this file.
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ * https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
  */
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
