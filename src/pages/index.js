4466675315f1bad6c76e1bec1255cba0b3ace910
Willian Antunes <willian.lima.antunes@gmail.com>
2021-01-26 18:54:57 -0300

    End of topic "Creating Nested Layout Components"

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from "../components/header"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 // export default function Home() {
 //   return <div>Hello my friend!</div>
@@ -20,13 +21,25 @@ import { Link } from "gatsby"
 //   );
 // }
 
+// export default function Home() {
+//   return (
+//     <div style={{ color: `purple` }}>
+//       <Link to="/contact/">Contact</Link>
+//       <Header headerText="Hello Gatsby!" />
+//       <p>What a world.</p>
+//       <img src="https://source.unsplash.com/random/400x200" alt="" />
+//     </div>
+//   )
+// }
+
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link>
-      <Header headerText="Hello Gatsby!" />
-      <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </div>
-  )
+    <Layout>
+      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+      </p>
+    </Layout>
+  );
 }
