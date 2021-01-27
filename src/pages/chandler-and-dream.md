3995d6a31c693bd5cedb06c2bebf46b6cb8ad051
Willian Antunes <willian.lima.antunes@gmail.com>
2021-01-27 08:45:04 -0300

    End of chapter "Transformer Plugins"

---
+++ src/pages/chandler-and-dream.md
@@ -0,0 +1,6 @@
+---
+title: "Chandler and his dream"
+date: "2021-01-27"
+---
+
+All right, so I'm back in high school. I'm standing in the middle of the cafeteria, and I realize I'm totally naked. Then I look down, and I realize there is a phone... There! All of a sudden, the phone starts to ring. Now I don't know what to do, everybody starts looking at me. Finally, I figure I'd better answer it, and it turns out it's my mother, which is very-very weird because, she never calls me!
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,61 @@
-// import React from "react"
-// import Header from "../components/header"
-// import { Link } from "gatsby"
-// import Layout from "../components/layout"
-
-// export default function Home() {
-//   return <div>Hello my friend!</div>
-// }
-
-// export default function Home() {
-//   return <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
-// }
-
-// export default function Home() {
-//   return (
-//     <div style={{ color: `purple` }}>
-//       <h1>Hello Gatsby!</h1>
-//       <p>What a world.</p>
-//     </div>
-//   );
-// }
-
-// export default function Home() {
-//   return (
-//     <div style={{ color: `purple` }}>
-//       <Link to="/contact/">Contact</Link>
-//       <Header headerText="Hello Gatsby!" />
-//       <p>What a world.</p>
-//       <img src="https://source.unsplash.com/random/400x200" alt="" />
-//     </div>
-//   )
-// }
-
-// export default function Home() {
-//   return (
-//     <Layout>
-//       <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-//       <p>
-//         What do I like to do? Lots of course but definitely enjoy building
-//         websites.
-//       </p>
-//     </Layout>
-//   );
-// }
-
 import React from "react"
+import { graphql } from "gatsby"
+import { css } from "@emotion/react"
+import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
 
-export default function Home () {
+export default function Home({ data }) {
+  console.log(data)
   return (
     <Layout>
-      <h1>Amazing Pandas Eating Things</h1>
       <div>
-        <img
-          src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-          alt="Group of pandas eating bamboo"
-        />
+        <h1
+          css={css`
+            display: inline-block;
+            border-bottom: 1px solid;
+          `}
+        >
+          Amazing Pandas Eating Things
+        </h1>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3
+              css={css`
+                margin-bottom: ${rhythm(1 / 4)};
+              `}
+            >
+              {node.frontmatter.title}{" "}
+              <span
+                css={css`
+                  color: #bbb;
+                `}
+              >
+                — {node.frontmatter.date}
+              </span>
+            </h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+    query {
+        allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+            totalCount
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        title
+                        date(formatString: "DD MMMM, YYYY")
+                    }
+                    excerpt
+                }
+            }
+        }
+    }
+`
